fb1f7db843d38d4c9729df4541d8870df268e121
Steve G <ssghostfacekillah@hotmail.com>
2022-10-08 21:24:13 +0100

    project complete

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,1 +1,11 @@
+# -*- coding: utf-8 -*-
+pound = u'\u00A3'
 
+print("Welcome to the tip calculator!\n")
+totalBill = input(f"What was the total bill? {pound}")
+tipPercent = input("What percentage tip would you like to give? ")
+totalPeople = input("How many people to split the bill? ")
+maths = (float(totalBill) / float(totalPeople)) * ((float(tipPercent)/100)+1)
+tip = round(maths, 2)
+
+print(f"\nEach person should pay: {pound}{tip}")
