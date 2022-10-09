ea4368ce0f24b4c6870a6f9c4a32b95d22a46593
Steve G <ssghostfacekillah@hotmail.com>
2022-10-09 03:23:22 +0100

    fix result format to 2 decimal places

--- TipCalculator.py
+++ TipCalculator.py
@@ -6,6 +6,6 @@ totalBill = input(f"What was the total bill? {pound}")
 tipPercent = input("What percentage tip would you like to give? ")
 totalPeople = input("How many people to split the bill? ")
 maths = (float(totalBill) / float(totalPeople)) * ((float(tipPercent)/100)+1)
-tip = round(maths, 2)
+tip = "{:.2f}".format(maths)
 
 print(f"\nEach person should pay: {pound}{tip}")
